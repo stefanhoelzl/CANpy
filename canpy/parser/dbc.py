e369df359c4c12c6d9c914ac457c291bbef52839
Stefan Hölzl <stefan.hoelzl@gmx.de>
2016-06-05 23:32:45 +0200

    parser: Initial commit for the DBC parser

---
+++ canpy/parser/dbc.py
@@ -0,0 +1,172 @@
+__author__ = "Stefan Hölzl"
+
+import re
+import collections
+
+from canpy.candb import CANDB, CANNode, CANMessage, CANSignal
+
+
+class DBCParser(object):
+    """Parses a DBC-file.
+    Follows docs/DBC_Specification.md"""
+    def __init__(self):
+        """Initializes the object"""
+        self._mode = ('NORMAL', None)
+        self._candb = CANDB()
+
+    # Method definitions
+    def parse_file(self, file_name):
+        """Parses a dbc file
+
+        Args:
+            file_name: Name of the file to parse.
+        Returns:
+            CANDB object
+        """
+        self._candb = CANDB()
+        with open(file_name, 'r') as dbc_fh:
+            for line in dbc_fh:
+                self._parse_line(line.strip())
+        return self._candb
+
+    def _parse_line(self, line):
+        """Parses one line of a dbc file and updates the CANDB
+
+        Args:
+            line: One line of a dbc file as string
+        Raises:
+            RuntimeError: If signal description is not in a message block
+        """
+        if line[:7] == 'VERSION':
+            self._candb .version = DBCParser._parse_version(line)
+            self._mode = ('NORMAL', None)
+        elif line[:3] == 'BU_':
+            node_names = DBCParser._parse_nodes(line)
+            for node_name in node_names:
+                self._candb.add_node(CANNode(node_name))
+            self._mode = ('NORMAL', None)
+        elif line[:3] == 'BO_':
+            md = DBCParser._parse_message(line)
+            message = CANMessage(md.can_id, md.name, md.length)
+            self._candb.get_node(md.sender).add_message(message)
+            self._mode = ('MESSAGE', message)
+        elif line[:3] == 'SG_':
+            if self._mode[0] != 'MESSAGE':
+                raise RuntimeError('Signal description not in message block')
+            sd = DBCParser._parse_signal(line)
+            signal = CANSignal(name=sd.name, start_bit=sd.start_bit, length=sd.length, little_endian=sd.little_endian,
+                               signed=sd.signed, factor=sd.factor, offset=sd.offset, value_min=sd.min_value,
+                               value_max=sd.max_value, unit=sd.unit,
+                               is_multiplexer=sd.is_multiplexer, multiplexer_id=sd.multiplexer_id)
+            for node_name in sd.receivers:
+                node = self._candb.get_node(node_name)
+                signal.add_receiver(node)
+            self._mode[1].add_signal(signal)
+        elif line[:3] == 'CM_':
+            dc = DBCParser._parse_description(line)
+            if dc.type == 'CANDB':
+                self._candb.description = dc.value
+            elif dc.type == 'NODE':
+                self._candb.get_node(dc.identifier).description = dc.value
+            elif dc.type == 'MESSAGE':
+                self._candb.get_message(dc.identifier).description = dc.value
+            elif dc.type == 'SIGNAL':
+                self._candb.get_signal(dc.identifier[0], dc.identifier[1]).description = dc.value
+            self._mode = ('NORMAL', None)
+
+    @staticmethod
+    def _parse_version(version_str):
+        """Parses a version string
+
+        Args:
+            version_str: String containing version informations
+        Returns:
+            Version from the verstion string
+        """
+        reg = re.search('VERSION\s*"(?P<version>\S+)"', version_str)
+        return reg.group('version')
+
+    @staticmethod
+    def _parse_nodes(nodes_str):
+        """Parses a nodes string
+
+        Args:
+            nodes_str: String containing nodes informations
+        Returns:
+            List with all the node names
+        """
+        reg = re.search('BU_\s*:(?P<nodes>.+)', nodes_str)
+        node_names_str = re.sub('\s+', ' ', reg.group('nodes')).strip()
+        return node_names_str.split(' ')
+
+    @staticmethod
+    def _parse_message(message_str):
+        """Parses a message string
+
+        Args:
+            message_str: String with message informations
+        Returns:
+            Namedtuple with can_id, name, length and sender name of the message
+        """
+        reg = re.search('BO_\s*(?P<can_id>\d+)\s*(?P<name>\S+)\s*:\s*(?P<length>\d+)\s*(?P<sender>\S+)', message_str)
+        cmd = collections.namedtuple('CANMessageDescription', 'can_id name length sender')
+        return cmd(can_id=int(reg.group('can_id')),name=reg.group('name').strip(),
+                   length=int(reg.group('length')), sender=reg.group('sender').strip())
+
+    @staticmethod
+    def _parse_signal(signal_str):
+        """Parses a signal string
+
+        Args:
+            signal_str: String with signal informations
+        Returns:
+            Namedtuple with the signal informations
+        """
+        pattern  = 'SG_\s*(?P<name>\S+)\s*(?P<is_multipexer>M)?(?P<multiplexer_id>m\d+)?\s*:\s*'
+        pattern += '(?P<start_bit>\d+)\|(?P<length>\d+)\@(?P<endianness>[0|1])(?P<sign>[\+|\-])\s*'
+        pattern += '\(\s*(?P<factor>\S+)\s*,\s*(?P<offset>\S+)\s*\)\s*\[\s*(?P<min_value>\S+)\s*\|\s*(?P<max_value>\S+)\s*\]'
+        pattern += '\s*"(?P<unit>\S*)"\s*(?P<receivers>.+)'
+        reg = re.search(pattern, signal_str)
+
+        little_endian = True if reg.group('endianness').strip() == '1' else False
+        signed = True if reg.group('sign').strip() == '-' else False
+        receivers = [receiver.strip() for receiver in re.sub('\s+', ' ', reg.group('receivers')).strip().split(' ')]
+        is_multiplexer = True if reg.group('is_multipexer') else False
+        multiplexer_id = int(reg.group('multiplexer_id').strip()[1:]) if reg.group('multiplexer_id') else None
+
+        csd = collections.namedtuple('CANSignalDescription', 'name start_bit length little_endian signed factor offset '
+                                                             'min_value max_value unit receivers '
+                                                             'is_multiplexer multiplexer_id')
+        return csd(name=reg.group('name').strip(), start_bit=int(reg.group('start_bit')), length=int(reg.group('length')),
+                   little_endian=little_endian, signed=signed, factor=float(reg.group('factor')),
+                   offset=float(reg.group('offset')), min_value=float(reg.group('min_value')),
+                   max_value=float(reg.group('max_value')), unit=reg.group('unit').strip(), receivers=receivers,
+                   is_multiplexer=is_multiplexer, multiplexer_id=multiplexer_id)
+
+    @staticmethod
+    def _parse_description(desc_str):
+        """Parses a description string
+
+        Args:
+            desc_str: String with description informations
+        Returns:
+            Namedtuple with value, type and identifier of the description
+        """
+        pattern  = 'CM_\s*(?P<node>BU_)?\s*(?P<msg>BO_)?\s*(?P<sig>SG_)?\s*'
+        pattern += '(?P<can_id>\d*)?\s*(?P<name>\S*)?\s*"(?P<value>.+)";'
+        reg = re.search(pattern, desc_str)
+
+        desc_type = 'CANDB'
+        desc_id = None
+        if reg.group('node'):
+            desc_type = 'NODE'
+            desc_id = reg.group('name').strip()
+        elif reg.group('msg'):
+            desc_type = 'MESSAGE'
+            desc_id = int(reg.group('can_id'))
+        elif reg.group('sig'):
+            desc_type = 'SIGNAL'
+            desc_id = (int(reg.group('can_id')), reg.group('name').strip())
+
+        dc = collections.namedtuple('DescriptionContainer', 'value type identifier')
+        return dc(value=reg.group('value'), type=desc_type, identifier=desc_id)
---
+++ tests/test_dbc_parser.py
@@ -0,0 +1,230 @@
+__author__ = "Stefan Hölzl"
+
+import pytest
+
+from canpy.candb import CANNode, CANMessage, CANSignal
+from canpy.parser.dbc import DBCParser
+
+testset_nodes = ['BU_   :   Node0   Node1    Node2', 'BU_:Node0 Node1 Node2']
+testset_version = ['VERSION "1.0"', 'VERSION    "1.0"']
+testset_message = ['BO_ 1234 CANMessage:8 Node0', 'BO_  1234  CANMessage  :  8  Node0']
+testset_signal =  ['SG_     Signal1     :    32|32@1+   (   33.3     ,  0    )  [0|100]     "%"     Node1   Node2',
+                   'SG_ Signal1:32|32@1+ (33.3,0) [ 0 | 100  ] "%" Node1 Node2']
+testset_signal_multiplexer = ['SG_ Signal1  M   :32|32@1+ (33.3,0)[0|100] "%" Node1 Node2']
+testset_signal_multiplexed = ['SG_ Signal1  m34  :32|32@1+ (33.3,0)[0|100] "%" Node1 Node2']
+testset_desc_candb = ['CM_ "The Description";', 'CM_    "The Description";']
+testset_desc_node = ['CM_ BU_ Node0 "The Description";', 'CM_    BU_   Node0    "The Description";']
+testset_desc_message = ['CM_ BO_ 1234 "The Description";', 'CM_    BO_   1234    "The Description";']
+testset_desc_signal = ['CM_ SG_ 1234 Signal0 "The Description";', 'CM_    SG_  1234   Signal0    "The Description";']
+
+class TestDBCParseLine(object):
+    @pytest.mark.parametrize("line", testset_version)
+    def test_parse_verison(self, line):
+        parser = DBCParser()
+        parser._parse_line(line)
+        assert parser._candb.version == "1.0"
+
+    @pytest.mark.parametrize("line", testset_nodes)
+    def test_parse_nodes(self, line):
+        parser = DBCParser()
+        parser._parse_line(line)
+        assert len(parser._candb.nodes) == 3
+        node_names = list(map(lambda n: n.name, parser._candb.nodes))
+        assert "Node0" in node_names
+        assert "Node1" in node_names
+        assert "Node2" in node_names
+
+    @pytest.mark.parametrize("line", testset_message)
+    def test_parse_message(self, line):
+        parser = DBCParser()
+        sender = CANNode('Node0')
+        parser._candb.add_node(sender)
+        parser._parse_line(line)
+        msg = parser._candb.nodes[0].messages[0]
+        assert msg.name == 'CANMessage'
+        assert msg.can_id == 1234
+        assert msg.length == 8
+        assert msg.sender == sender
+
+    @pytest.mark.parametrize("line", testset_signal)
+    def test_parse_signal(self, line):
+        parser = DBCParser()
+        node0 = CANNode('Node0')
+        parser._candb.add_node(node0)
+        node1 = CANNode('Node1')
+        parser._candb.add_node(node1)
+        node2 = CANNode('Node2')
+        parser._candb.add_node(node2)
+        msg = CANMessage(1, 'CANMessage', 8)
+        node0.add_message(msg)
+        parser._mode = ('MESSAGE', msg)
+        parser._parse_line(line)
+        sig = msg.signals[0]
+        assert sig.name == 'Signal1'
+        assert sig.start_bit == 32
+        assert sig.length == 32
+        assert sig.little_endian == True
+        assert sig.signed == False
+        assert sig.factor == 33.3
+        assert sig.value_min == 0
+        assert sig.value_max == 100
+        assert sig.unit == '%'
+        assert sig.is_multiplexer == False
+        assert sig.multiplexer_id == None
+        assert node1 in sig.receiver
+        assert node2 in sig.receiver
+
+    def test_parse_signal_wrong_block(self):
+        parser = DBCParser()
+        node = CANNode('Node0')
+        parser._mode = ('NORMAL', None)
+        with pytest.raises(RuntimeError):
+            parser._parse_line(testset_signal[0])
+
+    @pytest.mark.parametrize("line", testset_signal_multiplexer)
+    def test_parse_multiplexed_signal(self, line):
+        parser = DBCParser()
+        node0 = CANNode('Node0')
+        msg = CANMessage(1, 'CANMessage', 8)
+        node0.add_message(msg)
+        node1 = CANNode('Node1')
+        parser._candb.add_node(node1)
+        node2 = CANNode('Node2')
+        parser._candb.add_node(node2)
+        parser._mode = ('MESSAGE', msg)
+        parser._parse_line(line)
+        sig = msg.signals[0]
+        assert sig.is_multiplexer == True
+        assert sig.multiplexer_id == None
+
+    @pytest.mark.parametrize("line", testset_signal_multiplexed)
+    def test_parse_multiplexer_signal(self, line):
+        parser = DBCParser()
+        node0 = CANNode('Node0')
+        msg = CANMessage(1, 'CANMessage', 8)
+        node0.add_message(msg)
+        node1 = CANNode('Node1')
+        parser._candb.add_node(node1)
+        node2 = CANNode('Node2')
+        parser._candb.add_node(node2)
+        parser._mode = ('MESSAGE', msg)
+        parser._parse_line(line)
+        sig = msg.signals[0]
+        assert sig.is_multiplexer == False
+        assert sig.multiplexer_id == 34
+
+    def test_parse_candb_desc(self):
+        parser = DBCParser()
+        parser._parse_line(testset_desc_candb[0])
+        assert parser._candb.description == "The Description"
+
+    def test_parse_candb_node(self):
+        parser = DBCParser()
+        parser._candb.add_node(CANNode('Node0'))
+        parser._parse_line(testset_desc_node[0])
+        assert parser._candb.nodes[0].description == "The Description"
+
+    def test_parse_candb_message(self):
+        parser = DBCParser()
+        node = CANNode('Node0')
+        node.add_message(CANMessage(1234, 'CANMessage', 8))
+        parser._candb.add_node(node)
+        parser._parse_line(testset_desc_message[0])
+        assert parser._candb.nodes[0].messages[0].description == "The Description"
+
+    def test_parse_candb_signal(self):
+        parser = DBCParser()
+        node = CANNode('Node0')
+        msg = CANMessage(1234, 'CANMessage', 8)
+        sig = CANSignal('Signal0', 0, 8)
+        msg.add_signal(sig)
+        node.add_message(msg)
+        parser._candb.add_node(node)
+        parser._parse_line(testset_desc_signal[0])
+        assert sig.description == "The Description"
+
+
+class TestDBCStringParser(object):
+    @pytest.mark.parametrize("line", testset_version)
+    def test_parse_version(self, line):
+        assert DBCParser._parse_version(line) == "1.0"
+
+    @pytest.mark.parametrize("line", testset_nodes)
+    def test_parse_nodes(self, line):
+        assert DBCParser._parse_nodes(line) == ["Node0", "Node1", "Node2"]
+
+    @pytest.mark.parametrize("line", testset_message)
+    def test_parse_message(self, line):
+        msg = DBCParser._parse_message(line)
+        assert msg.can_id == 1234
+        assert msg.name == 'CANMessage'
+        assert msg.length == 8
+        assert msg.sender == 'Node0'
+
+    @pytest.mark.parametrize("line", testset_signal)
+    def test_parse_signal(self, line):
+        sig = DBCParser._parse_signal(line)
+        assert sig.name == 'Signal1'
+        assert sig.start_bit == 32
+        assert sig.length == 32
+        assert sig.little_endian == True
+        assert sig.signed == False
+        assert sig.factor == 33.3
+        assert sig.min_value == 0
+        assert sig.max_value == 100
+        assert sig.unit == '%'
+        assert sig.receivers == ['Node1', 'Node2']
+        assert sig.is_multiplexer == False
+        assert sig.multiplexer_id == None
+
+    def test_parse_signal_no_unit_signed(self):
+        sig = DBCParser._parse_signal('SG_ Signal0 : 0|32@1- (1,0) [0|0] "" Node1 Node2')
+        assert sig.signed == True
+        assert sig.unit == ""
+
+    @pytest.mark.parametrize("line", testset_signal_multiplexer)
+    def test_parse_multiplexer_signal(self, line):
+        sig = DBCParser._parse_signal(line)
+        assert sig.is_multiplexer == True
+        assert sig.multiplexer_id == None
+
+
+    @pytest.mark.parametrize("line", testset_signal_multiplexed)
+    def test_parse_multiplexed_signal(self, line):
+        sig = DBCParser._parse_signal(line)
+        assert sig.is_multiplexer == False
+        assert sig.multiplexer_id == 34
+
+    @pytest.mark.parametrize("line", testset_desc_candb)
+    def test_parse_candb_desc(self, line):
+        desc = DBCParser._parse_description(line)
+        assert desc.value == "The Description"
+        assert desc.type == "CANDB"
+        assert desc.identifier == None
+
+    @pytest.mark.parametrize("line", testset_desc_node)
+    def test_parse_candb_node(self, line):
+        desc = DBCParser._parse_description(line)
+        assert desc.value == "The Description"
+        assert desc.type == "NODE"
+        assert desc.identifier == 'Node0'
+
+    @pytest.mark.parametrize("line", testset_desc_message)
+    def test_parse_candb_message(self, line):
+        desc = DBCParser._parse_description(line)
+        assert desc.value == "The Description"
+        assert desc.type == "MESSAGE"
+        assert desc.identifier == 1234
+
+    @pytest.mark.parametrize("line", testset_desc_signal)
+    def test_parse_candb_signal(self, line):
+        desc = DBCParser._parse_description(line)
+        assert desc.value == "The Description"
+        assert desc.type == "SIGNAL"
+        assert desc.identifier == (1234, 'Signal0')
+
+def test_whole_dbc():
+    parser = DBCParser()
+    candb = parser.parse_file('tests/test.dbc')
+    assert len(candb.nodes) == 3
+    assert candb.version == "1.0"
