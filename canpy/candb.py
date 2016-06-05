e37d15497b0b5fa5ff89e55e5acb3cb7142e8a54
Stefan Hölzl <stefan.hoelzl@gmx.de>
2016-06-05 23:31:49 +0200

    candb: Initial commit for the CANDB module

---
+++ canpy/candb.py
@@ -0,0 +1,189 @@
+__author__ = "Stefan Hölzl"
+
+
+class CANDB(object):
+    """Representation of a CAN-Bus"""
+    def __init__(self):
+        """Initializes the object"""
+        self._nodes = []
+
+        self.version = ""
+        self.description = ""
+
+    # Property definitions
+    @property
+    def nodes(self):
+        return self._nodes
+
+    # Method definitions
+    def add_node(self, node):
+        """Adds a new Node to the CANDB
+
+        Args:
+            node: Node to add.
+        """
+        self._nodes.append(node)
+
+    def get_node(self, name):
+        """Returns node by name
+
+        Args:
+            name: Node name
+        Returns:
+            node with the given name
+        """
+        return [node for node in self._nodes if node.name == name][0]
+
+    def get_message(self, can_id=None, name=None):
+        """Returns message by can_id or name
+
+        Args:
+            can_id: Message CAN-ID
+            name:   Message name
+        Raises:
+            AttributeError: If weather can_id or name is given
+        Returns:
+            message with the given can-id or name
+        """
+        if can_id == None and name == None:
+            raise AttributeError('Either name or can_id attribute must be given')
+        if can_id:
+            return [msg for node in self.nodes for msg in node.messages if msg.can_id == can_id][0]
+        elif name:
+            return [msg for node in self.nodes for msg in node.messages if msg.name == name][0]
+        return None
+
+    def get_signal(self, can_id, name):
+        """Returns signal by name and can_id
+
+        Args:
+            can_id: CAN-ID of the message which contains the signal
+            name:   Signal name
+        Returns:
+            signal with the given name and CAN-ID
+        """
+        return [sig for node in self.nodes
+                        for msg in node.messages if msg.can_id == can_id
+                            for sig in msg.signals if sig.name == name][0]
+
+    # Protocol definitions
+    def __str__(self, *args, **kwargs):
+        return 'CANDB(Version: {}, Nodes: {}, Description: {})'.format(self.version, len(self.nodes), self.description)
+
+
+class CANNode(object):
+    """Representation of a CAN-Node"""
+    def __init__(self, name):
+        """Initializes the object
+
+        Args:
+            name: Name of the Node
+        """
+        self._messages = []
+
+        self.name = name
+        self.description = ""
+
+    # Property definitions
+    @property
+    def messages(self):
+        return self._messages
+
+    # Method definitions
+    def add_message(self, message):
+        """Add a new message to the Node.
+
+        Args:
+            message: Message to add
+        Raises:
+            RuntimeError: If message already belongs to a node
+        """
+        if message.sender:
+            raise RuntimeError('Message already belongs to node {}!'.format(message.sender))
+
+        message.sender = self
+        self._messages.append(message)
+
+    # Protocol definitions
+    def __str__(self, *args, **kwargs):
+        return 'CANNode(Name: {}, Messages: {}, Description: {})'.format(self.name, len(self.messages), self.description)
+
+class CANMessage(object):
+    """Represents a CAN-Message"""
+    def __init__(self, can_id, name, length):
+        """Initializes the object
+
+        Args:
+            can_id: CAN-ID of the message.
+            name:   Name of the message.
+            length: Message length in bytes
+        """
+        self._signals = []
+
+        self.can_id = can_id
+        self.name = name
+        self.length = length
+
+        self.description = ""
+        self.sender = None
+
+    @property
+    def signals(self):
+        return self._signals
+
+    # Method definitions
+    def add_signal(self, signal):
+        """Adds a signal to the message
+
+        Args:
+            signal: Signal to add
+        Raises:
+            RuntimeError: If signal already belongs to a message
+        """
+        if signal.message:
+            raise RuntimeError('Signal already belongs to message {}'.format(signal.message))
+        self._signals.append(signal)
+
+    # Protocol definitions
+    def __str__(self, *args, **kwargs):
+        return 'CANMessage(CAN-ID: {}, Name: {}, Length: {})'.format(self.can_id, self.name, self.length)
+
+class CANSignal(object):
+    """Represents a CAN-Signal"""
+    def __init__(self, name, start_bit, length, little_endian=True, signed=False, factor=1.0, offset=0.0, value_min=0,
+                 value_max=0, unit="", is_multiplexer=False, multiplexer_id=None):
+        self._receiver = []
+
+        self.name = name
+        self.start_bit = start_bit
+        self.length = length
+        self.little_endian  = little_endian
+        self.signed = signed
+        self.factor = factor
+        self.offset = offset
+        self.value_min = value_min
+        self.value_max = value_max
+        self.unit = unit
+        self.is_multiplexer = is_multiplexer
+        self.multiplexer_id = multiplexer_id
+
+        self.message = None
+        self.description = ""
+
+    # Property definitions
+    @property
+    def receiver(self):
+        return self._receiver
+
+    # Method definitions
+    def add_receiver(self, node):
+        """Adds a new receiver to the signal
+
+        Args:
+            node: Node which receives this message
+        """
+        self._receiver.append(node)
+
+    # Protocol definitions
+    def __str__(self, *args, **kwargs):
+        return 'CANSignal(Name: {}, Start: {}, Length: {})'.format(self.name, self.start_bit, self.length)
---
+++ tests/test_candb.py
@@ -0,0 +1,91 @@
+__author__ = "Stefan Hölzl"
+
+import pytest
+
+from canpy.candb import CANDB, CANMessage, CANNode, CANSignal
+
+class TestCANDB(object):
+    def test_add_node(self):
+        cdb = CANDB()
+        cdb.add_node(CANNode('TestNode'))
+        assert len(cdb.nodes) == 1
+
+    def test_get_node(self):
+        cdb = CANDB()
+        node = CANNode('TestNode')
+        cdb.add_node(node)
+        assert cdb.get_node('TestNode') == node
+
+    def test_get_message_by_can_id(self):
+        cdb = CANDB()
+        node = CANNode('TestNode')
+        msg = CANMessage(1234, 'Message', 8)
+        node.add_message(msg)
+        cdb.add_node(node)
+        assert cdb.get_message(can_id=1234) == msg
+
+    def test_get_message_by_name(self):
+        cdb = CANDB()
+        node = CANNode('TestNode')
+        msg = CANMessage(1234, 'Message', 8)
+        node.add_message(msg)
+        cdb.add_node(node)
+        assert cdb.get_message(name='Message') == msg
+
+    def test_get_message_no_parameter(self):
+        cdb = CANDB()
+        node = CANNode('TestNode')
+        msg = CANMessage(1234, 'Message', 8)
+        node.add_message(msg)
+        cdb.add_node(node)
+        with pytest.raises(AttributeError):
+            cdb.get_message()
+
+    def test_get_signal_by_name(self):
+        cdb = CANDB()
+        node = CANNode('TestNode')
+        msg = CANMessage(1234, 'Message', 8)
+        node.add_message(msg)
+        sig = CANSignal('Signal', 0, 8)
+        msg.add_signal(sig)
+        cdb.add_node(node)
+        assert cdb.get_signal(can_id=1234, name='Signal') == sig
+
+
+class TestCANNode(object):
+    def test_add_message(self):
+        node = CANNode('TestNode')
+        node.add_message(CANMessage(1, 'Name', 6))
+        assert len(node.messages) == 1
+
+    def test_set_message_sender(self):
+        node = CANNode('TestNode')
+        node.add_message(CANMessage(1, 'Name', 8))
+        assert node.messages[0].sender == node
+
+    def test_add_message_with_sender(self):
+        msg = CANMessage(1, 'Name', 8)
+        msg.sender = CANNode('TestNode')
+        node = CANNode('TestNode')
+        with pytest.raises(RuntimeError):
+            node.add_message(msg)
+
+class TestCANMessage(object):
+    def test_add_signal(self):
+        msg = CANMessage(1, 'Name', 8)
+        msg.add_signal(CANSignal('Signal', 0, 8))
+        assert len(msg.signals) == 1
+
+    def test_add_signal_with_message(self):
+        sig = CANSignal('Signal', 0, 8)
+        sig.message = CANMessage(1, 'Name', 8)
+        msg = CANMessage(2, 'Name', 8)
+        with pytest.raises(RuntimeError):
+            msg.add_signal(sig)
+
+class TestCANSignal(object):
+    def test_add_receiver(self):
+        node = CANNode('TestNode')
+        sig = CANSignal('Signal', 0, 8)
+        sig.add_receiver(node)
+        assert len(sig.receiver) == 1
