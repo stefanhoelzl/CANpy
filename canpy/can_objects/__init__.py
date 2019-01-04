56c3b395a82305e0e8a617a4d16c39fc3548f1e7
Stefan Hoelzl <stefan.hoelzl@posteo.de>
2019-01-04 16:58:13 +0100

    canpy: sources deleted

--- canpy/can_objects/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = "Stefan Hölzl"
-
-from canpy.can_objects.can_object import CANObject
-from canpy.can_objects.can_network import CANNetwork
-from canpy.can_objects.can_node import CANNode
-from canpy.can_objects.can_message import CANMessage
-from canpy.can_objects.can_signal import CANSignal
-from canpy.can_objects.can_attribute import CANAttribute
